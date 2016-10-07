5a03446896970e684cc7f350db1720193c8a9cbe
Christian Smith <cesm@umich.edu>
2016-10-07 14:56:12 -0400

    Final commit; doesn't work

--- rpn.py
+++ rpn.py
@@ -1,20 +1,47 @@
 #!/usr/bin/env python3
 
+import operator
+
+operators = {
+  '+': operator.add,
+  '-': operator.sub,
+  '*': operator.mul,
+  '/': operator.truediv,
+}
+
 def calculate(s):
   stack = []
   for token in s.split():
     if token == '+':
-      arg1 = stack.pop()
       arg2 = stack.pop()
+      arg1 = stack.pop()
       result = arg1 + arg2
+      stack.append(result)
+    elif token == '-':
+      arg2 = stack.pop()
+      arg1 = stack.pop()
+      result = arg1 - arg2
+      stack.append(result)
+    elif token == '*':
+      arg2 = stack.pop()
+      arg1 = stack.pop()
+      result = arg1 * arg2
+      stack.append(result)
+    elif token == '/':
+      arg2 = stack.pop()
+      arg1 = stack.pop()
+      result = arg1 / arg2
       stack.append(result)
     else:
       stack.append(int(token))
+  if len(stack) != 1:
+    raise TypeError
   return stack.pop()
 
 def main():
   while True:
-    calculate(input('rpn calc> '))
+    result = calculate(input('rpn calc> '))
+    print(result)
 
 if __name__ == '__main__':
   main()
