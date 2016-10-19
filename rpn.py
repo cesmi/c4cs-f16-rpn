6a1fd5f54da39760487a6bde483b3e0f6a696eb8
Christian Smith <cesm@umich.edu>
2016-10-18 22:37:47 -0400

    Added colorization for advanced hw5

--- rpn.py
+++ rpn.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 
 import operator
-
-# To demonstrate test coverage
-def factorial(x):
-  if x <= 1:
-    return 1
-  return x * factorial(x - 1)
+import readline
+from colorama import init, Fore, Back, Style
 
 operators = {
   '+': operator.add,
@@ -16,6 +12,57 @@ operators = {
   '^': operator.pow,
   '%': operator.mod
 }
+
+def isInteger(str):
+  try:
+    int(str)
+    return True
+  except ValueError:
+    return False
+
+def isFloat(str):
+  try:
+    float(str)
+    return True
+  except ValueError:
+    return False
+
+def colorize(output):
+  result = '';
+  for word in output.split():
+
+    if len(result):
+      result = result + Fore.RESET + Back.RESET + ' '
+
+    # Operators are yellow
+    if (word in operators):
+      result = result + Fore.YELLOW + Back.RESET + word
+      continue
+
+    # Ints are blue
+    elif (isInteger(word)):
+      if (int(word) >= 0):
+        result = result + Fore.BLUE + Back.RESET + word
+      else:
+        result = result + Fore.BLUE + Back.RED + word
+
+    # Floats are magenta
+    elif (isFloat(word)):
+      if (float(word) >= 0):
+        result = result + Fore.MAGENTA + Back.RESET + word
+      else:
+        result = result + Fore.MAGENTA + Back.RED + word
+
+    else:
+      result = result + Fore.RESET + Back.RESET + word
+
+  return result
+
+# To demonstrate test coverage
+def factorial(x):
+  if x <= 1:
+    return 1
+  return x * factorial(x - 1)
 
 def calculate(myarg):
   stack = list()
@@ -34,9 +81,12 @@ def calculate(myarg):
   return stack.pop()
 
 def main():
+  init(autoreset=True)
   while True:
-    result = calculate(input("rpn calc> "))
-    print(result)
+    userInput = input("rpn calc> ")
+    result = calculate(userInput)
+    print(colorize(userInput))
+    print(colorize(str(result)))
 
 if __name__ == '__main__':
   main()
